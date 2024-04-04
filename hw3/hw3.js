c0f3ad857f8b6c6a38c845267160ab8a20c69f3f
NikaKhi <xaiminik@gmail.com>
2024-04-04 21:35:22 +0300

    corrections

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -1,6 +1,6 @@
 //ЗАДАНИЕ 1
 let password = "Пароль";
-let answer = promt("Введите пароль");
+let answer = prompt("Введите пароль");
 if (password === answer) {
     alert("Пароль введен верно");
 }
@@ -9,28 +9,27 @@ else {
 }
 
 //ЗАДАНИЕ 2
-let c = Number(promt("Введите любое число"));
+let c = 5;
 if (c > 0 && c < 10) {
-    alert("Верно");
-}
-else {
-    alert("Неверно");
+console.log(‘Верно’);
+} else {
+console.log(‘Неверно’);
 }
 
 //ЗАДАНИЕ 3
-let b = 3;
-let e = 5;
+const b = 42;
+const e = 103;
 if (b > 100 || e > 100) {
-    alert("Верно");
-}
-else {
-    alert("Неверно");
+console.log(“Верно”);
+} else {
+console.log(“Неверно”);
 }
 
 //ЗАДАНИЕ 4
-let a = 2;
-let t = 3;
-alert(a + b);
+let a = '2';
+let t = '3';
+let q = a + t;
+alert(typeof(q));
 
 //ЗАДАНИЕ 5
 let monthNumber = Number(prompt("Введите номер месяца"));
