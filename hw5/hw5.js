7206eb997df7688ebd9a3b30cb687ab029cd5a0f
NikaKhi <xaiminik@gmail.com>
2024-05-12 19:29:52 +0300

    const Math.PI

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -77,10 +77,10 @@ console.log(cube());
 
 // ЗАДАНИЕ 7
 function getCircleArea() {
-return 3.14 * this.radius ** 2;
+return Math.PI * this.radius ** 2;
 }
 function getCirclePerimeter() {
-return 2 * this.radius * 3.14;
+return 2 * this.radius * Math.PI;
 }
 const circle1 = {
 radius: 5,
